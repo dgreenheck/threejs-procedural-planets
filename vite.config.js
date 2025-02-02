e9375532d4c0f4344cefe25cd01250e8720d8f94
Daniel Greenheck <dgreenheck@gmail.com>
2025-02-02 14:47:17 -0600

    Clear vite config

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,4 @@
 * @type {import('vite').UserConfig}
 */
 export default {
-  // Set the base directory for GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '/threejs-procedural-planets/' : '/',
-  build: {
-    outDir: './dist',
-    sourcemap: true,
-  },
-  publicDir: './public',
 }
